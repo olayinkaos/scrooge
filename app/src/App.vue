c2fcc564e4cf1c525089318d8367d4e81c222a9e
Olayinka Success <olayinka.omole@gmail.com>
2017-12-19 00:15:24 +0100

    adding base views

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <div id="app">
-    <router-view/>
+    <section class="section">
+      <div class="container">
+        <router-view/>
+      </div>
+    </section>
   </div>
 </template>
 
@@ -12,11 +16,11 @@ export default {
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  /* font-family: 'Avenir', Helvetica, Arial, sans-serif; */
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  /* text-align: center; */
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 0 100px;
 }
 </style>
--- app/src/components/Home.vue
+++ app/src/components/Home.vue
@@ -1,17 +1,46 @@
 <template>
-  <div class="home">
-    <h1>Welcome, I'll be your Scrooge today 👿</h1>
-  </div>
+	<div class="home">
+		<h1 class="title">
+			Welcome, <br>
+			I'll be your Scrooge today 👿
+		</h1>
+		<div class="columns">
+			<div class="column">
+				<form action="">
+					<div class="field">
+						<div class="control">
+							<input class="input" type="number" placeholder="Enter expenditure">
+						</div>
+					</div>
+					<div class="field">
+						<div class="control">
+							<textarea class="textarea" name="" id="" cols="30" rows="3" placeholder="Description (Optional)"></textarea>
+						</div>
+					</div>
+					<div class="field is-grouped">
+						<button class="button is-link is-fullwidth">Submit</button>
+					</div>
+				</form>
+			</div>
+			<div class="column">
+
+			</div>
+			<div class="column">
+
+			</div>
+		</div>
+
+	</div>
 </template>
 
 <script>
 export default {
-  name: 'Home',
-  data () {
-    return {
-      like: 'dat'
-    }
-  }
+	name: 'Home',
+	data () {
+		return {
+			like: 'dat'
+		}
+	}
 }
 </script>
 
